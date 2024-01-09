2f1ba99c3bd82d1a9663bf50ccbfbc617b8047e1
ricky7171 <samuel.ricky@ti.ukdw.ac.id>
2024-01-09 07:32:32 +0700

    remove unused function

--- helper/helper.go
+++ helper/helper.go
@@ -16,18 +16,3 @@ func SpecialAddition(num1, num2 int) int {
 	}
 
 }
-
-func SpecialSubstraction(num1, num2 int) int {
-	res := 0
-	if num2%2 == 0 {
-		res = num1 - num2 - 7
-	} else {
-		res = num1 - num2 + 1
-	}
-	if res < 0 {
-		return 0
-	} else {
-		return res
-	}
-
-}
